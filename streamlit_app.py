f3b11476a390476372a1c91684ff263578a14160
yurikgorges <yurikgorges@gmail.com>
2024-06-19 21:46:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,13 @@ import streamlit as st
 
 st.title("Finanças pessoais v0.1")
 
+st.selectbox('O que fazer?','Inserir novo pagamnento','Visualizar pagamentos já feitos')
 
-novo_pag = st.button('Inserir um novo pagamento')
+
 bd_pags = st.button('Visualizar pagamentos já feitos')
 
-novo_pag = ("teste")
 
-print(novo_pag)
+
 
 
 # st.selectbox('Escolha a forma de pagamento',['Cartão BB','Cartão Nubank','Dinheiro/PIX'])
