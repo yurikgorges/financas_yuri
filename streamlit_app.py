ea32502c02a3ff547cea102556e4b314173563ce
yurikgorges <yurikgorges@gmail.com>
2024-06-19 21:19:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
@@ -9,6 +8,7 @@ import streamlit as st
 """
 texto = st.write("Hello, im trying something here")
 
+print(texto)
 
 
 
@@ -20,30 +20,3 @@ texto = st.write("Hello, im trying something here")
 
 
 
-
-#num_points = st.slider("Number of points in spiral", 1, 1000, 1100)
-#num_turns = st.slider("Number of turns in spiral", 1, 200, 31)
-
-#indices = np.linspace(0, 1, num_points)
-#theta = 2 * np.pi * num_turns * indices
-#radius = indices
-
-#x = radius * np.cos(theta)
-#y = radius * np.sin(theta)
-
-#df = pd.DataFrame({
-   # "x": x,
-    #"y": y,
-   # "idx": indices,
-   # "rand": np.random.randn(num_points),
-#})
-
-#st.altair_chart(alt.Chart(df, height=700, width=700)
-   # .mark_point(filled=True)
-   # .encode(
-   #     x=alt.X("x", axis=None),
-   #     y=alt.Y("y", axis=None),
-  #      color=alt.Color("idx", legend=None, scale=alt.Scale()),
-   #     size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-  #  ))
-#
